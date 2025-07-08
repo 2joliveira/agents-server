2598ac19e8bc0d7a256bc7f06719fb8ba6f0d7b5
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-07-08 11:44:34 -0300

    adjust seed

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -4,7 +4,17 @@ import { schema } from "./schemas/index.ts";
 
 await reset(db, schema);
 
-await seed(db, schema);
+await seed(db, schema).refine((f) => {
+  return {
+    rooms: {
+      count: 20,
+      columns: {
+        name: f.companyName(),
+        description: f.loremIpsum(),
+      },
+    },
+  };
+});
 
 await sql.end();
 
