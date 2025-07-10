9cc5b5c8d397801aca282fe8540c7474eb82c19a
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-07-10 20:25:24 -0300

    create question route

--- src/server.ts
+++ src/server.ts
@@ -9,12 +9,13 @@ import { env } from "./env.ts";
 import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 import { createRoomRoute } from "./http/routes/create-room.ts";
 import { getRoomsQuestion } from "./http/routes/get-room-questions.ts";
+import { createQuestionRoute } from "./http/routes/create-question.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-// app.register(fastifyCors, {
-//   origin: "http://localhost:5173",
-// });
+app.register(fastifyCors, {
+  origin: "http://localhost:5173",
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -22,6 +23,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(getRoomsRoute);
 app.register(createRoomRoute);
 app.register(getRoomsQuestion);
+app.register(createQuestionRoute);
 
 app.listen({ port: env.PORT }).then(() => {
   console.log("HTTP server running !!");
